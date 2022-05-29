a998d5abcbaa566e7aaf084856f89fb53ad72657
QiuLi22 <104408128+QiuLi22@users.noreply.github.com>
2022-05-28 22:02:49 -0700

    Initial commit

---
+++ InventoryMaintenance/InvItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMaintenance
+{
+    public class InvItem
+    {
+        public InvItem() { }
+        public InvItem(int itemNo, string description, decimal price)
+        {
+            ItemNo = itemNo;
+            Description = description;
+            Price = price;
+        }
+        public int ItemNo { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public string GetDisplayText()
+        {
+            return $"{ItemNo}    {Description}({Price.ToString("C2")}) ";
+        }
+    }
+}
